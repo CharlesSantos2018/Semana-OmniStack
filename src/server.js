34e713faabb5a06f4ff99a8ce971c0aec2151e51
CharlesSantos <charles-henrique2011@hotmail.com>
2019-04-15 17:05:42 -0300

    ajustado app.use(cors());

--- src/server.js
+++ src/server.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
     return next();
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
